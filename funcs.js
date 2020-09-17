37866167819f5c156ac6adeac05ce5cfb4915538
ryangood1 <62087744+ryangood1@users.noreply.github.com>
2020-09-17 14:32:04 +1200

    Add files via upload

--- funcs.js
+++ funcs.js
@@ -1,1 +1,11 @@
-ds
+function confirmHealth() {
+  if(document.getElementById("yes").checked == true) {
+    window.location.href = "index.html";
+  }
+  else {
+    var div = document.getElementById("health");
+    var reply = document.createElement("p");
+    reply.innerHTML = "We're sorry, but you should only be Ride Sharing if you are healthy enough to do so, for your own and for all our users safety and well-being.";
+    div.append(reply);
+  }
+}
