49869eda37e0ab19c1e79fa4fc79c9c9fe5dfd1e
ryangood1 <62087744+ryangood1@users.noreply.github.com>
2020-10-20 14:59:14 +1300

    Add files via upload

--- funcs.js
+++ funcs.js
@@ -15,11 +15,12 @@ function getOffers() {
   var healthDiv = document.getElementById("ridesContainer");
   healthDiv.innerHTML = "";
   var url = "getOffers.php";
-  ajaxRequest("GET", url, displayOffers);
+  var data = "";
+  ajaxRequest("GET", url, data, displayOffers);
 }
 
 //This is the ajaxRequest object that handles requests asynchronously
-function ajaxRequest(method, url, callback) {
+function ajaxRequest(method, url, data, callback) {
 
   let request = new XMLHttpRequest();
   request.open(method, url, true);
@@ -34,22 +35,59 @@ function ajaxRequest(method, url, callback) {
         response = request.responseText;
         callback(response);
       } else {
-        alert("Error");
+        alert("Error in Ajax Request");
       }
     }
   };
-  request.send();
+  request.send(data);
 }
 
 function displayOffers(response) {
-  //If the response is -1, then the "Rides" database is empty
-  var healthDiv = document.getElementById("ridesContainer");
-  if(response == -1) {
-    var reply = document.createElement("p").innerHTML = "There are not current offers at this time";
-    healthDiv.appendChild(reply);
-  }
-  else {
-    var output = document.createElement("p").innerHTML = response;
-    healthDiv.appendChild(output);
-  }
+  var responseArray = JSON.parse(response);
+  var ridesContainerDiv = document.getElementById("ridesContainer");
+
+  //for(var i = 0; i < responseArray.length; i++) {
+    //var specArray = responseArray[i];
+
+    //Getting values from JSON array into variables
+    var driver_name = responseArray.driver_name;
+    var passenger_name = responseArray.passenger_name;
+    var ride_type = responseArray.ride_type;
+    var leave_time = responseArray.leave_time;
+    var pickup_time = responseArray.pickup_time;
+    var current_location = responseArray.current_location;
+    var destination = responseArray.destination;
+
+    var offerDiv = document.createElement("div");
+    offerDiv.classList = "offerDiv";
+    var output1 = document.createElement("p");
+    var textNode1 = document.createTextNode("Driver: " + driver_name);
+    var output2 = document.createElement("p");
+    var textNode2 = document.createTextNode("Passenger: " + passenger_name);
+    var output3 = document.createElement("p");
+    var textNode3 = document.createTextNode("Ride Type: " + ride_type);
+    var output4 = document.createElement("p");
+    var textNode4 = document.createTextNode("Leave Time: " + leave_time);
+    var output5 = document.createElement("p");
+    var textNode5 = document.createTextNode("Pickup Time: " + pickup_time);
+    var output6 = document.createElement("p");
+    var textNode6 = document.createTextNode("Current Location: " + current_location);
+    var output7 = document.createElement("p");
+    var textNode7 = document.createTextNode("Destination: " + destination);
+    output1.appendChild(textNode1);
+    output2.appendChild(textNode2);
+    output3.appendChild(textNode3);
+    output4.appendChild(textNode4);
+    output5.appendChild(textNode5);
+    output6.appendChild(textNode6);
+    output7.appendChild(textNode7);
+    offerDiv.appendChild(output1);
+    offerDiv.appendChild(output2);
+    offerDiv.appendChild(output3);
+    offerDiv.appendChild(output4);
+    offerDiv.appendChild(output5);
+    offerDiv.appendChild(output6);
+    offerDiv.appendChild(output7);
+
+    ridesContainerDiv.appendChild(offerDiv);
 }
---
+++ getOffers.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "mysql.rideshare.hamwebs.com";
+$username = "waikato";
+$password = "@bGYpRSE5@";
+$dbname = "rideshare_hamwebs";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$query = "select * from rides";
+
+	$result = $conn->query($query);
+
+	while($row = $result->fetch()){
+			//If table is empty, return -1
+			if($row == "") {
+				echo -1;
+				return;
+			}
+			else {
+				//Return the details of the ride offers
+				$offerData = array("driver_name"=>$row['driver_name'], "passenger_name"=>$row['passenger_name'], "ride_type"=>$row['ride_type'], "leave_time"=>$row['leave_time'],
+				"pickup_time"=>$row['pickup_time'], "current_location"=>$row['current_location'], "destination"=>$row['destination']);
+				echo json_encode($offerData);
+			}
+	}
+	return;
+}
+catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
