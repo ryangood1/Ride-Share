c8d1c43822ffb9ab4f5816fe93b365428bae933b
AJW8 <65757145+AJW8@users.noreply.github.com>
2020-10-28 17:15:01 +1300

    Update submitRequest.php

--- submitRequest.php
+++ submitRequest.php
@@ -4,37 +4,41 @@
 	$password = "@bGYpRSE5@";
 	$dbname = "rideshare_hamwebs";
 	$role = $_POST['role'];
-	$name = $_SESSION['FULLNAME'];
-	if($role == "driver"){
-		$driver = $name;
-		$passenger = "";
-	}
-	else if($role == "passenger"){
-		$driver = "";
-		$passenger = $name;
-	}
-	else{
-		$driver = "";
-		$passenger = "";
-	}
 	$startTime = $_POST['startTime'];
 	$endTime = $_POST['endTime'];
 	$from = $_POST['from'];
 	$to = $_POST['to'];
 	$party = $_POST['party'];
 	$contact = $_POST['contact'];
-	$conn = mysqli_connect($servername, $username, $password, $dbname);
-	if(!$conn) {
+	session_start();
+	$con = mysqli_connect($servername, $username, $password, $dbname);
+	if(!$con){
 		die("Connection failed: " . mysqli_connect_error());
 	}
-	echo "Connected successfully";
-	$sql = "INSERT INTO Rides (driver_name, passenger_name, ride_type, leave_time, pickup_time, current_location, destination, party, contact) VALUES ($driver, $passenger, $role, $startTime, $endTime, $from, $to, $party, $contact)";
-	if(mysqli_query($conn, $sql)) {
-		echo "New record created successfully";
+	echo "Connected successfully.";
+	$email = $_SESSION['ue'];
+	$sql = "SELECT name FROM user_profile where email="$email";
+	$result = mysqli_query($con, $sql);
+	if ($result->num_rows == 1){
+		while($row = $result->fetch_assoc()){
+			$name = $row;
+		}
+	}
+	if($role == "Driver"){
+		$driver = $name;
+		$passenger = "";
 	}
 	else{
-		echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		$driver = "";
+		$passenger = $name;
 	}
-	mysqli_close($conn);
+	$sql = "INSERT INTO rides (driver_name, passenger_name, ride_type, leave_time, pickup_time, current_location, destination, party, contact) VALUES ($driver, $passenger, $role, $startTime, $endTime, $from, $to, $party, $contact)";
+	if(mysqli_query($con, $sql)) {
+		echo "New record created successfully.";
+	}
+	else{
+		echo "Error: " . $sql . "<br>" . mysqli_error($con);
+	}
+	mysqli_close($con);
 	echo "<script>window.location.replace("https://www.rideshare.hamwebs.com/main.php");</script>";
 ?>
